e1bfe9d466fe3a7800c04e246c806537aed3b6fc
hemangiaghera <117673828+hemangiaghera@users.noreply.github.com>
2022-12-23 10:48:25 +0530

    Add files via upload

---
+++ Concatnation_DNAseq.py
@@ -0,0 +1,6 @@
+fst_dna="atgcatgcatgcatgc"
+print("First DNA: " + fst_dna)
+sec_dna="atgcatgcatgcatgc"
+print("Second DNA:" + sec_dna)
+comp_dna=fst_dna+sec_dna
+print("complete DNA sequence is:" + comp_dna)
